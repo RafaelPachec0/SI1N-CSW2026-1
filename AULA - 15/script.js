457eb52c34c46f4af8f708bb8db1d309278fa42a
RafaelPachec0 <rafaeldasilvapacheco877@gmail.com>
2026-05-22 20:54:23 -0300

    Add files via upload

---
+++ AULA - 15/script.js
@@ -0,0 +1,25 @@
+document.writeln('Olá Mundo!!!');
+
+window.alert('Olá Mundo, seu lindo!');
+
+document.getElementById('Título').innerHTML = "Olá Mundo JavaScript";
+
+let nomePrompt = window.prompt('Olá Aluno de JavaScript, qual o seu nome?');
+
+document.getElementById('nomeAluno').innerHTML = ("O nome do aluno JavaScript é: " + nomePrompt);
+
+let resposta = false;
+
+if (resposta) {
+    document.getElementById('nomeAluno').innerHTML = 'O aluno gosta de JavaScript'
+}
+else {
+    document.getElementById('nomeAluno').innerHTML = 'O aluno NÃO GOSTA de JavaScript'
+}
+
+function exibeNomeAluno() {
+    let inputAluno = document.getElementById('inputNomeAluno').value;
+    document.getElementById('resposta').innerHTML = "O nome digitado foi: " + inputAluno;
+}
+
+exibeNomeAluno()
