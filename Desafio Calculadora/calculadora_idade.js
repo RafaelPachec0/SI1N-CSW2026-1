8bc99f15acaed184d12f9945474cb504b1edbc16
RafaelPachec0 <rafaeldasilvapacheco877@gmail.com>
2026-05-28 19:15:51 -0300

    Add files via upload

---
+++ Desafio Calculadora/calculadora_idade.js
@@ -0,0 +1,13 @@
+function calcularIdade() {
+    var ano_atual = 2026
+
+    var anoNascimento = document.getElementById("anoNascimento").value
+
+    var idade = ano_atual - anoNascimento
+
+    document.getElementById("resultado").innerHTML = `Você tem ${idade} anos.`
+
+}
+
+var 
+let 
