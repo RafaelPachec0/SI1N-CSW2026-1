5b9f273302d9cf6b57c6910f8ad9a24e01a9fa59
RafaelPachec0 <rafaeldasilvapacheco877@gmail.com>
2026-05-28 21:58:33 -0300

    Add files via upload

--- Desafio Calculadora/calculadora_idade.js
+++ Desafio Calculadora/calculadora_idade.js
@@ -1,13 +1,36 @@
 function calcularIdade() {
-    var ano_atual = 2026
+    const diaInput = parseInt(document.getElementById('dia').value);
+    const mesInput = parseInt(document.getElementById('meses').value);
+    const anoInput = parseInt(document.getElementById('anoNascimento').value);
+    
+    if (!diaInput || !anoInput) {
+        window.alert("Algum campo não foi preenchido.");
+        return;
+    }
 
-    var anoNascimento = document.getElementById("anoNascimento").value
+    const hoje = new Date();
+    const anoAtual = hoje.getFullYear();
+    const mesAtual = hoje.getMonth() + 1;
+    const diaAtual = hoje.getDate();
 
-    var idade = ano_atual - anoNascimento
-
-    document.getElementById("resultado").innerHTML = `Você tem ${idade} anos.`
-
+    let idade = anoAtual - anoInput;
+    
+    if (mesAtual < mesInput || (mesAtual === mesInput && diaAtual < diaInput)) {
+        idade--;
+    }
+    document.getElementById('resultado').innerText = "A sua idade é: " + idade + " anos.";
 }
 
-var 
-let 
+function calcularNumerica() {
+    let num1 = document.getElementById('num1').value
+    let num2 = document.getElementById('num2').value
+    let operacao =document.getElementById('operacao').value
+    let resultado = 0
+
+    switch(op) {
+        case "+" : resultado = Number(num1) + Number(num2); break;
+        case "-" : resultado = Number(num1) - Number(num2); break;
+        case "*" : resultado = Number(num1) * Number(num2); break;
+    }
+
+}
